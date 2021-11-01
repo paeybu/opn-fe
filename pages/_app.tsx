27f142660d6a75da2cdfd51f51af3045a0dd0a2c
Paekabu <pae9769@gmail.com>
2021-11-01 09:27:15 +0700

    init and  setup chakra and theme, and axios

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,25 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import { ChakraProvider } from '@chakra-ui/react';
+import { extendTheme } from '@chakra-ui/react';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const colors = {
+    bg: '#012d5e',
+    fontColor: '#ffffff',
+    opnBlue: '#5882E3',
+    opnLightBlue: '#254870',
+    opnYellow: '#ffc107',
+    opnRed: '#dc3545',
+    opnWhite: '#ffffff',
+  };
+  const theme = extendTheme({ colors });
+
+ return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
 export default MyApp
